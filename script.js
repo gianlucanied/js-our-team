61680ac13cbc307a3d7e59cac9207e8040881e56
gianlucanied <gianluca.nieddu96@gmail.com>
2023-12-01 15:18:55 +0100

    Add: array membri del team

---
+++ script.js
@@ -0,0 +1,45 @@
+/* 
+Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team. Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite.
+MILESTONE 1:
+Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+MILESTONE 2:
+Stampare le stesse informazioni su DOM sotto forma di stringhe */
+
+// Milestone 0
+
+const arrayMembriTeam = [
+    {
+        "nomeCognome": "Wayne Barnett",
+        "role": "Founder & CEO",
+        "image": "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        "nomeCognome": "Angela Caroll",
+        "role": "Chief Editor",
+        "image": "angela-caroll-chief-editor.jpg"
+    },
+    {
+        "nomeCognome": "Walter Gordon",
+        "role": "Office Manager",
+        "image": "walter-gordon-office-manager.jpg"
+    },
+    {
+        "nomeCognome": "Angela Lopez",
+        "role": "Social Media Manager",
+        "image": "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        "nomeCognome": "Scott Estrada",
+        "role": "Developer",
+        "image": "Scott-estrada-developer.jpg"
+    },
+    {
+        "nomeCognome": "Barbara Ramos",
+        "role": "Graphic Designer",
+        "image": "barbara-ramos-graphic-designer.jpg"
+    }
+];
+
+console.log(arrayMembriTeam);
